420651175aed2806f42c7a998ff648a94e250531
Zen <PlumeriaGardens@zens-macbook-air.local>
2017-07-09 17:31:55 -0700

    Initial Commit

---
+++ CampusMeme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CampusMeme
+//
+//  Created by Zen Simone on 7/9/17.
+//  Copyright © 2017 zs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
